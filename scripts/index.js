ceb86191bacc34c31b2ed541f56a016930be8f61
BlacknoSheep <wanghaokanade@gmial.com>
2023-09-23 10:42:11 +0800

    fix enter fade out

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,7 @@ import rain from "./rain.js";
 const main = document.querySelector("#main");
 function finishFn() {
     //   main.style.display = "block";
+    main.style.transition = "opacity 2s ease-in-out";
     main.style.opacity = "1";
 }
 const canvas = document.querySelector("#rain");
